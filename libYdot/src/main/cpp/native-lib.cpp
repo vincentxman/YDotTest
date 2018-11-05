647d80227532e934ee204429268981367c9a78af
vincentxman <vincentxman@hotmail.com>
2018-11-05 09:22:44 +0800

    import libYdot

---
+++ libYdot/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_cdgo_libydot_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++, Vincent";
+    return env->NewStringUTF(hello.c_str());
+}
