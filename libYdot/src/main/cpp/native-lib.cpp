1369001117db7cfcfd28ec3f0aaebb132d3fa99f
vincentxman <vincentxman@hotmail.com>
2018-11-05 09:56:14 +0800

    移除 lib 内的 MainActivity

--- libYdot/src/main/cpp/native-lib.cpp
+++ libYdot/src/main/cpp/native-lib.cpp
@@ -2,14 +2,6 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_cdgo_libydot_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++, Vincent";
-    return env->NewStringUTF(hello.c_str());
-}
-
-extern "C" JNIEXPORT jstring JNICALL
 Java_com_cdgo_libydot_LibX_stringFromJNI(
         JNIEnv *env,
 jobject /* this */) {
